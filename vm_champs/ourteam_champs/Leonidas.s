5ebed81376dac7277fd5be95893678d1c4a3bcd4
Gian Brandon stark <gbrandon@student.42.fr>
2019-12-27 22:58:29 +0300

    add simple champion

--- vm_champs/ourteam_champs/Leonidas.s
+++ vm_champs/ourteam_champs/Leonidas.s
@@ -1,14 +1,24 @@
 .name		"Leonidas, warrior-king of Sparta"
 .comment	"This is SPARTAAAA!"
 
-live:
+	ld		%16777216, r15
+	ld		%167770880, r14
+	ld		%00000036, r2
+	ld		%00000008, r3
+	ld		%-327680, r4
+	ld		%524288, r5
+loop:
+
+	add		r4, r5, r4
+	add		r2, r3, r2
+	ld		%0, r16
+	sti		r15, r2, %0
+	sti		r1, r2, %-5
+	sti		r14, r2, %-7
 	st		r1, 6
 	live	%99
-	ld		%0, r16
-	fork	%7
-	zjmp	%:live
-	ld		%:-4, r3  ; write that error in 11:16
-	st		r3, %:copy
-	ld		%-18, r3
-	st		r3, %:copy
-copy:
+	fork	%6
+	zjmp	%:loop
+endloop:
+	st		r4, 6
+	zjmp	%99
